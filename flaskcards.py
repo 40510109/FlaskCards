f6bb9a16f0eeed0a3d4f5865920a5f8e884d24f7
Marc Turley <40510109@live.napier.ac.uk>
2020-12-01 13:34:20 +0000

    tidied up and added some comments

--- flaskcards.py
+++ flaskcards.py
@@ -8,23 +8,23 @@ app =  Flask(__name__)
 def root():
 	return render_template('home.html'), 200
 
-@app.route("/language/<langid>/")
+@app.route("/language/<langid>/") #set game options template
 def language(langid):
 	return render_template('lang.html', languageid=langid), 200
 
-@app.route("/language/<langid>/<category>/<diff>")
+@app.route("/language/<langid>/<category>/<diff>") #main
 def egame(langid, category, diff):
 	with open("static/langdata/" + langid + "/" + langid + category + ".txt") as file:
-		dict = [line.rstrip() for line in file]
+		dict = [line.rstrip() for line in file] #read file with language data into an array
 	with open("static/langdata/english/english" + category + ".txt") as qfile:
-		ENGLISH = [line.rstrip() for line in qfile]
-	rIndex=randrange(len(ENGLISH))
+		ENGLISH = [line.rstrip() for line in qfile] 
+	rIndex=randrange(len(ENGLISH)) #generate random number from 1 to max number of lines in file so not to go out of bounds
 	qCard=ENGLISH[rIndex]
 	aCard=dict[rIndex]
-	if diff == "2":
-	        rSample=random.sample(list(dict),2)
-	        rSample.append(aCard)
-	        random.shuffle(rSample)
+	if diff == "2": #if difficulty is 2/easy generate 3 cards
+	        rSample=random.sample(list(dict),2) #pick 2 random cards and assign to array
+	        rSample.append(aCard) #append the correct card to the array
+	        random.shuffle(rSample) #shuffle the array before passing to template
 	        return render_template('egame.html', langid=langid, cat=category, qCard=qCard, aCard=aCard, rSample=rSample), 200
 	elif diff == "5":
 	        rSample=random.sample(list(dict),5)
@@ -36,26 +36,11 @@ def egame(langid, category, diff):
                 rSample.append(aCard)
                 random.shuffle(rSample)
                 return render_template('hgame.html', langid=langid, cat=category, qCard=qCard, aCard=aCard, rSample=rSample), 200
-        #app.logger.info('\n Logging Start \n random_index=' + random_index1 + '\n random_index2=' + random_index2)
-	#app.logger.info('random_index:' + str(random_index))
-	#app.logger.info('random_index1:' + str(random_index1))
-	#app.logger.info('random_index2:' + str(random_index2))
-	#app.logger.info('qCard:' + str(qCard))
-	#app.logger.info('aCard:' + str(aCard))
-	#app.logger.info('rSample:' + str(rSample))
-	#app.logger.info('langid:'+ str(langid))
-	#app.logger.info('eng len:' + str(len(ENGLISH)))
-	#app.logger.info('ger len:' + str(len(GERMAN)))
-	#app.logger.info('type: langid:' + str(type(langid)))
-
-@app.route("/test/<firstname>/<surname>")
-def test(firstname, surname):
-	return "Name:" + firstname + surname
+	#app.logger.info('\n debug \n rIndex=' + rIndex + '\n qCard=' + qCard + '\n aCard=' + aCard)  ##debugging
 
 @app.errorhandler(404)
 def not_found():
-	return "Page not found"
-#    return make_response(render_template("404.html"), 404)
+   	return make_response(render_template("404.html"), 404)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
